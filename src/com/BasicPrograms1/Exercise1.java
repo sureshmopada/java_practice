356b1795780b66af4595613f2d2b676a330d863a
suresh <sureshmopada322@gmail.com>
2025-01-25 20:08:24 +0530

    first commit - basic java files

---
+++ src/com/BasicPrograms1/Exercise1.java
@@ -0,0 +1,7 @@
+package com.BasicPrograms1;
+
+public class Exercise1 {
+    public static void main(String[] args) {
+        System.out.println("Hello\nSuresh");
+    }
+}
---
+++ src/com/BasicPrograms1/Exercise2.java
@@ -0,0 +1,11 @@
+package com.BasicPrograms1;
+
+public class Exercise2 {
+    public static void main(String[] args) {
+        int num1 = 74;
+        int num2 = 36;
+        int sumOfTwoNums = num1 + num2;
+        System.out.println(sumOfTwoNums);
+
+    }
+}
